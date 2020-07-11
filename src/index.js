d0fc6495ee8e667393c8220733e25e490d969ce9
Ughonue Lawrence <lawrenceughonu@yahoo.com>
2020-07-11 23:53:57 +0100

    created nav bar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,84 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const Word = { Message: 'I am the tallest man in the world' }
+const App = (props) => {
+  return (
+    <div style={Styles.Stuff} >
+        1
+      <button style={Styles.eee}>
+      <i className="fa fa-exclamation-triangle" aria-hidden="true" style = {Styles.good}/>
+      {props.Word.Message}
+      </button>
+      <div id = 'Timer' className = 'Timer' />
+    </div>
+  )
+}
+const Styles = {
+  eee: {
+    color: 'red',
+    height: '6em',
+    width: '35em',
+    display: 'flex',
+    padding: '1em',
+    margin: '8em auto',
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    borderRadius: '0.8em'
+  },
+  good: {
+    // height: '4em',
+    // backgroundColor: 'green'
+    fontSize: '3em',
+  },
+  
+  Stuff: {
+    margin: 'auto',
+    fontSize: '4em',
+    textAlign: 'center'
+  }
+}
+
+
+
+
+// Creating a Nav bar
+let children = [
+  ['/', 'Home'],
+  ['/about', 'About'],
+  ['/contact', 'Contact']
+]
+const Nav = ({ children }) => {
+  let items = (children);
+    items = items.map(item => (
+      <div >
+        <a href={`${item[0]}`}>
+          {item[1]}
+        </a>
+      </div>));
+  console.log(items);
+  return (
+    <div
+      style={{
+      fontSize:'3em'
+    }}
+    >
+      {items}
+    </div>
+  ) 
+}
+
+
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <>
+  <App Word={Word} />
+  <Nav children={children}/>
+</>
+,
+  document.querySelector('#root')
+)
